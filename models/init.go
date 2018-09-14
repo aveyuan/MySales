5a4bd359978f35d4c62cf5b47008e5bb093ae200
aveyuan <aveyuan@163.com>
2018-09-14 10:07:46 +0800

    添加对Sqlite3的支持

--- models/init.go
+++ models/init.go
@@ -5,28 +5,42 @@ import (
 	"net/url"
 	"github.com/astaxie/beego/orm"
 	_"github.com/go-sql-driver/mysql"
-
+	_"github.com/mattn/go-sqlite3"
+	"strings"
+	"os"
 )
 
 func Init()  {
+	beego.Info("正在初始化数据库配置.")
+	adapter := beego.AppConfig.String("db_adapter")
 	prefix := beego.AppConfig.String("db.prefix")
-	dbhost := beego.AppConfig.String("db.host")
-	dbport := beego.AppConfig.String("db.port")
-	dbuser := beego.AppConfig.String("db.user")
-	dbpassword := beego.AppConfig.String("db.password")
-	dbname := beego.AppConfig.String("db.name")
-	timezone := beego.AppConfig.String("db.timezone")
-	if dbport == "" {
-		dbport = "3306"
+	//如果配置的是mysql
+	if strings.EqualFold(adapter, "mysql") {
+		dbhost := beego.AppConfig.String("db.host")
+		dbport := beego.AppConfig.String("db.port")
+		dbuser := beego.AppConfig.String("db.user")
+		dbpassword := beego.AppConfig.String("db.password")
+		dbname := beego.AppConfig.String("db.name")
+		timezone := beego.AppConfig.String("db.timezone")
+		if dbport == "" {
+			dbport = "3306"
+		}
+		dsn := dbuser + ":" + dbpassword + "@tcp(" + dbhost + ":" + dbport + ")/" + dbname + "?charset=utf8"
+		if timezone != "" {
+			dsn = dsn + "&loc=" + url.QueryEscape(timezone)
+		}
+		orm.RegisterDataBase("default", "mysql", dsn)
+		orm.RegisterDriver("default", orm.DRMySQL)
+	}else if strings.EqualFold(adapter, "sqlite3"){
+		orm.RegisterDriver("sqlite",orm.DRSqlite)
+		orm.RegisterDataBase("default","sqlite3","sales_data.db")
+	}else {
+		beego.Error("不支持的数据库类型.")
+		os.Exit(1)
 	}
-	dsn := dbuser + ":" + dbpassword + "@tcp(" + dbhost + ":" + dbport + ")/" + dbname + "?charset=utf8"
-	if timezone != "" {
-		dsn = dsn + "&loc=" + url.QueryEscape(timezone)
-	}
-	orm.RegisterDataBase("default", "mysql", dsn)
-	orm.RegisterDriver("default",orm.DRMySQL)
 	orm.RegisterModelWithPrefix(prefix,new(Client),new(Product),new(Sales),new(User),new(Salespd))
 	if beego.AppConfig.String("runmode") == "dev" {
 		orm.Debug = true
 	}
+	beego.Info("数据库初始化完成.")
 }
