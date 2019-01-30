d356102f0ad53f24ca0934f94143eefce9c0d393
= <=>
2019-01-30 09:37:28 +0800

    修复订单按照客户名称查询不显示问题

--- models/sales.go
+++ models/sales.go
@@ -1,6 +1,8 @@
 package models
 
-import "github.com/astaxie/beego/orm"
+import (
+	"github.com/astaxie/beego/orm"
+)
 
 //销售数据库
 /*
@@ -45,9 +47,16 @@ func (this *Sales)ListLimit(limit,page int,key string)([]*Sales,[]*Sales)  {
 	}else {
 		con := orm.NewCondition()
 		con1 := con.Or("Id__icontains",key).Or("SalesPhone__icontains",key).Or("SalesAddress__icontains",key)
+		//分页数据显示
 		o.QueryTable(Sales{}).SetCond(con1).Limit(limit,(page-1)*limit).OrderBy("-Id").OrderBy("-Id").All(&saless)
+		//总共数据
 		o.QueryTable(Sales{}).SetCond(con1).OrderBy("-Id").All(&num)
 
+	}
+	//如果没有查询到，再尝试是否是通过姓名来查询的，这里需要反向查询
+	if len(num)==0{
+		o.QueryTable(Sales{}).Filter("Client__Name", key).RelatedSel().Limit(limit,(page-1)*limit).OrderBy("-Id").OrderBy("-Id").All(&saless)
+		o.QueryTable(Sales{}).Filter("Client__Name", key).OrderBy("-Id").All(&num)
 	}
 	return saless,num
 }
